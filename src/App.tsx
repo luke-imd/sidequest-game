070e9e46f3dc2baf85b837eaa0a262bea7386e86
luke-imd <luke.ziegler@imdbiotech.com>
2025-05-22 13:33:04 +0200

    fixed tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 
 function App() {
-
   return (
     <>
-        <div >
-            <h1 className="text-3xl font-bold underline text-">    Hello world!  </h1>
-        </div>
+      <div className="bg-slate-800 p-8 min-h-screen">
+        <h1 className="text-red-500 underline italic text-3xl mb-4">Hello world!</h1>
+        <h1 className="text-blue-500 underline italic text-3xl mb-4">Hello world!</h1>
+        <p className="text-green-500">This text should be green</p>
+        <p className="text-yellow-500">This text should be yellow</p>
+        <button className="bg-purple-600 text-white p-2 rounded mt-4 hover:text-green-500">Button</button>
+      </div>
     </>
   )
 }
